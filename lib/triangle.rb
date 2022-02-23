a1e914a4e02ec0341babdf66f4532979f29e6b58
Mark Momot <markmomot03@gmail.com>
2022-02-23 18:05:15 -0500

    commit tree d4d0647e21a47debb89cac7dcc1c4b0a8fabe77e

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,43 @@
 class Triangle
-  # write code here
+  attr_reader :side1, :side2, :side3
+
+  def initialize (side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    if check_triangle
+      if (self.side1 == self.side2) && (self.side1 == self.side3)
+        :equilateral
+      elsif (self.side1 == self.side2) || (self.side1 == self.side3) || (self.side2 == self.side3)
+        :isosceles
+      else
+        :scalene
+      end
+    else
+      raise TriangleError
+    end 
+  end
+
+
+
+  class TriangleError < StandardError
+
+  end
+
+  private
+
+  def check_triangle
+    if self.side1 > 0 && self.side2 > 0 && self.side3 > 0
+      if self.side1 + self.side2 > self.side3 && self.side1 + self.side3 > self.side2 && self.side2 + self.side3 > self.side1
+        true
+      else
+        false
+      end
+    else
+      false
+    end
+  end
 end
